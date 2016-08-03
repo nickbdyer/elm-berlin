bbd53b31b24e020874c1d6ab6488f7a67bf696c6
Nick Dyer <nbdyer@gmail.com>
2016-08-03 14:12:47 +0100

    Test passing for 5 minutes

--- src/Berlin.elm
+++ src/Berlin.elm
@@ -9,4 +9,11 @@ getSingleMinutes stringTime =
   Date.fromString stringTime
     |> Result.withDefault (Date.fromTime 0)
     |> Date.minute
+    |> moduloFive
 
+
+modulo =
+  (\num mod -> num % mod)
+
+moduloFive =
+  (flip modulo) 5
