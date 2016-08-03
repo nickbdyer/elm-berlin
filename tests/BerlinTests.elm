df04c984569337b5fc8739796755af9a263dacbb
Nick Dyer <nbdyer@gmail.com>
2016-08-03 14:04:02 +0100

    Passes normal minute calculations, but failing for 5 minutes

--- tests/BerlinTests.elm
+++ tests/BerlinTests.elm
@@ -12,9 +12,24 @@ tests =
       <| assertEqual 0 (Berlin.getSingleMinutes("1970/01/01 00:00:00"))
 
     , test 
-      "Given a time 00:01:00, the single minutes row has 0 illuminations" 
+      "Given a time 00:01:00, the single minutes row has 1 illuminations" 
       <| assertEqual 1 (Berlin.getSingleMinutes("1970/01/01 00:01:00"))
 
+    , test 
+      "Given a time 00:02:00, the single minutes row has 2 illuminations" 
+      <| assertEqual 2 (Berlin.getSingleMinutes("1970/01/01 00:02:00"))
+
+    , test 
+      "Given a time 00:03:00, the single minutes row has 3 illuminations" 
+      <| assertEqual 3 (Berlin.getSingleMinutes("1970/01/01 00:03:00"))
+
+    , test 
+      "Given a time 00:04:00, the single minutes row has 4 illuminations" 
+      <| assertEqual 4 (Berlin.getSingleMinutes("1970/01/01 00:04:00"))
+
+    , test 
+      "Given a time 00:05:00, the single minutes row has 0 illuminations" 
+      <| assertEqual 0 (Berlin.getSingleMinutes("1970/01/01 00:05:00"))
     ]
 
 
