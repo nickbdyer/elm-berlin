65746b46bbbc3d11e6fe406ea408ed806ff33ac5
Nick Dyer <nbdyer@gmail.com>
2016-08-04 11:17:04 +0100

    Adds string building method, first test passing for minutes

--- src/Berlin.elm
+++ src/Berlin.elm
@@ -2,54 +2,57 @@ module Berlin exposing (getSingleMinutes, getSingleHours, getSeconds, getFiveMin
 
 
 import Date exposing (fromString)
+import String exposing (repeat)
 
-getSingleMinutes : String -> Int
+
+getSingleMinutes : String -> String
 getSingleMinutes stringTime =
-  Date.fromString stringTime
-    |> Result.withDefault (Date.fromTime 0)
-    |> Date.minute
-    |> moduloFive
-
+  getRow stringTime Date.minute moduloFive
+    |> makeString 4 "Y"
+ 
 
 getFiveMinutes : String -> Int
 getFiveMinutes stringTime =
-  Date.fromString stringTime
-    |> Result.withDefault (Date.fromTime 0)
-    |> Date.minute
-    |> (\y x -> x // y) 5
+  getRow stringTime Date.minute divideFive
 
 
 getSingleHours : String -> Int
 getSingleHours stringTime =
-  Date.fromString stringTime
-    |> Result.withDefault (Date.fromTime 0)
-    |> Date.hour
-    |> moduloFive
+  getRow stringTime Date.hour moduloFive
     
 
 getFiveHours : String -> Int
 getFiveHours stringTime =
-  Date.fromString stringTime
-    |> Result.withDefault (Date.fromTime 0)
-    |> Date.hour
-    |> (\y x -> x // y) 5
+  getRow stringTime Date.hour divideFive
 
 
 getSeconds : String -> Int
 getSeconds stringTime =
+  getRow stringTime Date.second illuminateSeconds
+
+
+getRow stringTime unitFunction mathsFunction =
   Date.fromString stringTime
     |> Result.withDefault (Date.fromTime 0)
-    |> Date.second
-    |> iluminateSeconds 
+    |> unitFunction
+    |> mathsFunction
 
 
-modulo =
-  (\num mod -> num % mod)
+makeString : Int -> String -> Int -> String
+makeString numLightsTotal lightPattern numLightsOn =
+  List.repeat numLightsTotal "O"
+    |> List.drop numLightsOn
+    |> List.append (List.repeat numLightsOn lightPattern) 
+    |> String.join ""
+
+
+divideFive =
+  (\divisor num -> num // divisor) 5
 
 moduloFive =
-  (flip modulo) 5
+  (\mod num -> num % mod) 5
 
-iluminateSeconds value = 
+illuminateSeconds value = 
   case value of
     0 -> 1
     value -> (value + 1) `rem` 2
--- tests/BerlinTests.elm
+++ tests/BerlinTests.elm
@@ -9,59 +9,27 @@ tests =
     [ 
       test 
       "Given a time 00:00:00, the single minutes row has 0 illuminations" 
-      <| assertEqual 0 (Berlin.getSingleMinutes("1970/01/01 00:00:00"))
+      <| assertEqual "OOOO" (Berlin.getSingleMinutes("1970/01/01 00:00:00"))
 
-    , test 
-      "Given a time 00:01:00, the single minutes row has 1 illuminations" 
-      <| assertEqual 1 (Berlin.getSingleMinutes("1970/01/01 00:01:00"))
+    --, test 
+      --"Given a time 00:03:00, the single minutes row has 3 illuminations" 
+      --<| assertEqual "RRRO" (Berlin.getSingleMinutes("1970/01/01 00:03:00"))
 
-    , test 
-      "Given a time 00:02:00, the single minutes row has 2 illuminations" 
-      <| assertEqual 2 (Berlin.getSingleMinutes("1970/01/01 00:02:00"))
-
-    , test 
-      "Given a time 00:03:00, the single minutes row has 3 illuminations" 
-      <| assertEqual 3 (Berlin.getSingleMinutes("1970/01/01 00:03:00"))
-
-    , test 
-      "Given a time 00:04:00, the single minutes row has 4 illuminations" 
-      <| assertEqual 4 (Berlin.getSingleMinutes("1970/01/01 00:04:00"))
-
-    , test 
-      "Given a time 00:05:00, the single minutes row has 0 illuminations" 
-      <| assertEqual 0 (Berlin.getSingleMinutes("1970/01/01 00:05:00"))
-
-    , test 
-      "Given a time 00:39:00, the single minutes row has 4 illuminations" 
-      <| assertEqual 4 (Berlin.getSingleMinutes("1970/01/01 00:39:00"))
+    --, test 
+      --"Given a time 00:05:00, the single minutes row has 0 illuminations" 
+      --<| assertEqual "OOOO" (Berlin.getSingleMinutes("1970/01/01 00:05:00"))
 
     , test 
       "Given a time 00:00:00, the single hours row has 0 illuminations" 
       <| assertEqual 0 (Berlin.getSingleHours("1970/01/01 00:00:00"))
-
-    , test
-      "Given a time 01:00:00, the single hours row has 1 illuminations" 
-      <| assertEqual 1 (Berlin.getSingleHours("1970/01/01 01:00:00"))
-
-    , test 
-      "Given a time 02:00:00, the single hours row has 2 illuminations" 
-      <| assertEqual 2 (Berlin.getSingleHours("1970/01/01 02:00:00"))
 
     , test 
       "Given a time 03:00:00, the single hours row has 3 illuminations" 
       <| assertEqual 3 (Berlin.getSingleHours("1970/01/01 03:00:00"))
 
     , test 
-      "Given a time 04:00:00, the single hours row has 4 illuminations" 
-      <| assertEqual 4 (Berlin.getSingleHours("1970/01/01 04:00:00"))
-
-    , test 
       "Given a time 05:00:00, the single hours row has 0 illuminations" 
       <| assertEqual 0 (Berlin.getSingleHours("1970/01/01 05:00:00"))
-
-    , test 
-      "Given a time 18:00:00, the single hours row has 4 illuminations" 
-      <| assertEqual 3 (Berlin.getSingleHours("1970/01/01 18:00:00"))
 
     , test 
       "Given a time 00:00:00, the seconds lamp has 1 illumination" 
@@ -106,10 +74,6 @@ tests =
     , test 
       "Given a time 14:00:00, the five hours lamps have 2 illuminations" 
       <| assertEqual 2 (Berlin.getFiveHours("1970/01/01 14:00:00"))
-
-    , test 
-      "Given a time 23:00:00, the five hours lamps have 4 illuminations" 
-      <| assertEqual 4 (Berlin.getFiveHours("1970/01/01 23:00:00"))
 
     , test 
       "Given a time 23:00:00, the five hours lamps have 4 illuminations" 
